9f46d05abc1ea633085fbc60cd39dfd6cf839c33
Jawed Alam <zjaweds@outlook.com>
2020-10-21 06:38:38 +0530

    Array data structure partial implementation using template class

--- arrayDataStructure.cpp
+++ arrayDataStructure.cpp
@@ -1,38 +1,57 @@
 #include<iostream>
-
+#include<string.h>
 using namespace std;
 
-class array{
-    int index;
+template <class T>class Array{
+    T *a;
+    int size;
     int length;
     public:
-    array(int l){
-        length=l;
-        index=0;
-    }
+    Array(int n);
     bool isEmpty();
-    int size();
-    void insert(float,int);
+    int getSize();
+    void insert(T x,int);
     void del(int i);
-    int indexOf(float);
+    int indexOf(T x);
     float get(int);
     void display();
 };
-
-bool array::isEmpty(){
-    if(index==0){
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+template <class T> Array<T>::Array(int n){
+    a= new T[n];
+        length =n;
+        size=0;
 }
 
+
+template
+<class T>
+void Array<T>::insert(T ob,int index){
+    if(size==length)
+        cout<<"Array is full"<<endl;
+    else if (index<0||index>size)
+        cout<<"Invalid index"<<endl;
+    else
+        for(int i=size-1;i>index;i--)
+            a[i+1]=a[i];
+    a[index]=ob;
+    size++;
+}
+template<class T>
+bool Array<T>::isEmpty(){
+    if(size==0)
+        return (bool)true;
+    else
+        return (bool)false;
+}
 int main(){
-    array x(10);
-    if(x.isEmpty()){
+    Array<int> x(10);
+    x.insert(4,0);
+    if(x.isEmpty())
         cout<<"Array is empty"<<endl;
+    else
+    {
+        cout<<"Array is not empty"<<endl;
     }
+    
     return 0;
 }
