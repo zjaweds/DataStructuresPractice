dcd325126d4bcdbab624e60e27a5a550747b8046
Jawed Alam <zjaweds@outlook.com>
2020-10-30 23:09:51 +0530

    Programme execution sucessfull but not every aspect explored

--- arrayDS.cpp
+++ arrayDS.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 #include<cstring>
-
 using namespace std;
-
 class Exception{
     char *s;
     public:
@@ -15,10 +13,7 @@ class Exception{
         return abc;
     }
 };
-
-template
-<class T>
-class Array{
+template <class T> class Array{
     T *x;
     int size,length;
     public:
@@ -35,7 +30,31 @@ class Array{
     T get(int index);
     void display();
 };
-
+template<class T>void Array<T>::display(){
+    ostream& operator<<(ostream &cba,T t){
+        cout<<t.size;
+        cout<<t.length;
+    }
+}
+template<class T>T Array<T>::get(int index){
+    if(index>length||index<0)
+        throw Exception("Invalid index");
+    return x[index];
+}
+template<class T> int Array<T>::indexOf(T t){
+    for(int i=0;i<=size;i++){
+        if(t==x[i])
+        return i;
+    }
+}
+template<class T>T Array<T>::del(int index){
+    if(index>length||index<0)
+        throw Exception("Invalid index");
+    T obj;
+    obj=x[index];
+    size--;
+    return obj;
+}
 template<class T> bool Array<T>::isEmpty(){
     if(size==0)
         return true;
@@ -78,9 +97,11 @@ int main(){
     Array <char*>student(5);
     student.insert("Student",0);
     student.insert("Name",1);
+    student.del(1);
     student.insert("is",2);
     //student.insert("Dell",6);
     cout<<student.getSize()<<endl;
+    cout<<"Index of student: "<<student.indexOf("Student")<<endl;
     Array<Person>p(3);
     cout<<p.getSize()<<endl;
     Person x("Student",34,"F"),r("Alpha",32,"NA");
